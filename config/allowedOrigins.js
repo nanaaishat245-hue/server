e8c916ad3bfaeb2375e55eca1f31d406f613c488
Aishat Abiodun <nanaaishat245@gmail.com>
2025-12-16 10:55:53 +0100

    vercel

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigin = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5000',
+    "https://client-zeta-eight-37.vercel.app"
 ];
 
 module.exports = allowedOrigin;
+cd ..allowedOrigin
